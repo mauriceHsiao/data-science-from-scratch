b9440d02a233e045e0dbb5da1cee2c39a62b19a5
Hsiao <10561111@gm.nfu.edu.tw>
2017-05-30 17:30:36 +0800

    0530

--- code/neural_networks.py
+++ code/neural_networks.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 from __future__ import division
 from collections import Counter
 from functools import partial
@@ -158,13 +159,13 @@ if __name__ == "__main__":
                 for c in row.strip()]
                 
     inputs = map(make_digit, raw_digits)
-
     targets = [[1 if i == j else 0 for i in range(10)]
                for j in range(10)]
-
+    print "target",targets
+    # 調整參數
     random.seed(0)   # to get repeatable results
     input_size = 25  # each input is a vector of length 25
-    num_hidden = 5   # we'll have 5 neurons in the hidden layer
+    num_hidden = 10   # we'll have 5 neurons in the hidden layer
     output_size = 10 # we need 10 outputs for each input
 
     # each hidden neuron has one weight per input, plus a bias weight
@@ -218,4 +219,3 @@ if __name__ == "__main__":
           ]
     print
 
-    
